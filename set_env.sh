8cd779b0eb64c2930fd76f2f68ac8a252116c677
jempitman <80392341+jempitman@users.noreply.github.com>
2022-05-13 15:55:44 +0200

    secure environment variables

--- set_env.sh
+++ set_env.sh
@@ -2,10 +2,10 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=password1
-export POSTGRES_HOST=udagram2.csixzufepzrt.us-west-2.rds.amazonaws.com
-export POSTGRES_DB=udagram
+export POSTGRES_HOST=udagrampitmandev.canbqk05qh03.eu-west-3.rds.amazonaws.com
+export POSTGRES_DB=udagrampitmandev
 export AWS_BUCKET=arn:aws:s3:::udaconnect
-export AWS_REGION=us-west-2
-export AWS_PROFILE=test
-export JWT_SECRET=testing
+export AWS_REGION=eu-west-3
+export AWS_PROFILE=default
+export JWT_SECRET=hello
 export URL=http://localhost:8100
